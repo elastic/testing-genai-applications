744db5fc2945101c87244b35f5e17272c0dfa78f
Anuraag Agrawal <anuraaga@gmail.com>
2025-06-13 11:07:38 +0900

    Better

--- 09-user-feedback/client.py
+++ 09-user-feedback/client.py
@@ -2,6 +2,7 @@
 # Copyright Elasticsearch B.V. and contributors
 # SPDX-License-Identifier: Apache-2.0
 #
+from contextlib import contextmanager
 import os
 import contextvars
 from openai import OpenAI
@@ -21,17 +22,32 @@ class ChatResponse:
 # We need the OpenTelemetry span ID created automatically by OpenInference
 # instrumentation. Since this instrumentation applies at a low level, the
 # `request` function, we need to patch some function called inside that.
-span_id_var = contextvars.ContextVar("span_id", default=0)
+span_id_holder_var = contextvars.ContextVar("span_id_holder")
 original_build_request = BaseClient._build_request
 
 
 def span_id_capturing_build_request(self, *args, **kwargs):
-    ctx = trace.get_current_span().get_span_context()
-    span_id_var.set(ctx.span_id)
+    span_id_holder = span_id_holder_var.get(None)
+    if span_id_holder is not None:
+        ctx = trace.get_current_span().get_span_context()
+        span_id_holder[0] = ctx.span_id
     return original_build_request(self, *args, **kwargs)
 
 
-BaseClient._build_request = span_id_capturing_build_request
+@contextmanager
+def capture_span_id():
+    span_id_holder = [0]
+    token = span_id_holder_var.set(span_id_holder)
+
+    def get_span_id():
+        return span_id_holder[0]
+
+    BaseClient._build_request = span_id_capturing_build_request
+    try:
+        yield get_span_id
+    finally:
+        BaseClient._build_request = original_build_request
+        span_id_holder_var.reset(token)
 
 
 class OpenAIClient:
@@ -48,9 +64,10 @@ class OpenAIClient:
                 "content": message,
             },
         ]
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=messages,
-        )
-        content = response.choices[0].message.content
-        return ChatResponse(content, span_id_var.get())
+        with capture_span_id() as get_span_id:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages,
+            )
+            content = response.choices[0].message.content
+            return ChatResponse(content, get_span_id())
